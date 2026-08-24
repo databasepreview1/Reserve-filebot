b0aa9d12dbd2e1d56ea6f4f4351bb81d69ea7322
databasepreview1 <databasepreview1@gmail.com>
2026-08-24 12:40:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ DB_URL = os.environ.get("DB_URL", "mongodb+srv://a:a@loude.0uy8em5.mongodb.net/?
 DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
 
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1004497933918"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003911182920"))
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 
 
